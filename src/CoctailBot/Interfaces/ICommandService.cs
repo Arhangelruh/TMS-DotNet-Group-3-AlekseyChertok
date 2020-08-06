b56f6e7b6447166ea025451fdcd304cbd51ab963
Aleksey <arhangelruh@gmail.com>
2020-08-06 16:28:15 +0300

    feach: logic from work whith TelegramBot

---
+++ src/CoctailBot/Interfaces/ICommandService.cs
@@ -0,0 +1,18 @@
+﻿using CoctailBot.Commands;
+using System.Collections.Generic;
+
+
+namespace CoctailBot.Interfaces
+{
+    /// <summary>
+    /// Commands management service.
+    /// </summary>
+    public interface ICommandService
+    {
+        /// <summary>
+        /// Get all available commands.
+        /// </summary>
+        /// <returns>Command list.</returns>
+        IEnumerable<ITelegramCommand> Get();
+    }
+}
---
+++ src/CoctailBot/Services/CommandService.cs
@@ -0,0 +1,26 @@
+﻿using CoctailBot.Commands;
+using CoctailBot.Interfaces;
+using System.Collections.Generic;
+
+namespace CoctailBot.Services
+{
+    /// <inheritdoc cref="ICommandService"/>
+    public class CommandService : ICommandService
+    {
+        private readonly IEnumerable<ITelegramCommand> _commands;
+
+        /// <summary>
+        /// Base constructor.
+        /// </summary>
+        public CommandService()
+        {
+            _commands = new List<ITelegramCommand>
+            {
+               // что то должно быть, наши команды
+            };
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<ITelegramCommand> Get() => _commands;
+    }
+}
