2a59185a2fb806bb8b4a9c82d5e7f33689f75080
DzmitryBandarenka <stibaslo.d@gmail.com>
2020-08-19 16:57:41 +0300

    feat: add command search by ID

---
+++ src/CoctailBot/Commands/IDCommand.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using CoctailBot.Interfaces;
+using CoctailBot.Resources;
+using CoctailBot.Services;
+using CoctailBot.Logics;
+
+namespace CoctailBot.Commands
+{
+    /// <inheritdoc cref="ITelegramCommand"/>
+    public class IDCommand : ITelegramCommand
+    {
+        /// <inheritdoc/>
+        public string Name { get; } = IDCocktails.Link;
+
+        /// <inheritdoc/>
+        public async Task Execute(Message message, ITelegramBotClient client)
+        {
+            IApiService workWithApi = new ApiService();
+            var chatId = message.Chat.Id;
+            var data = message.Text;
+            var coctails = workWithApi.GetCocktailsByID(data).GetAwaiter().GetResult();
+            await client.SendTextMessageAsync(chatId, $"\nCocktail ID:");
+            foreach (var coctail in coctails)
+            {
+                await client.SendTextMessageAsync(chatId, $"Coctail Name: {coctail.strDrink}");
+
+                GetIngridients getIngridients = new GetIngridients();
+                string coctailIngridient = "Coctail ingridients: ";
+                var ingridients = getIngridients.GetListIngridientsAsync(coctail).GetAwaiter().GetResult();
+                foreach (var ingridient in ingridients)
+                {
+                    coctailIngridient += ingridient + "; ";
+                }
+
+                await client.SendTextMessageAsync(chatId, $"\n{coctailIngridient}");
+                await client.SendTextMessageAsync(chatId, $"\nInstructions: \n{coctail.strInstructions} \n{coctail.strDrinkThumb}");
+            }
+        }
+
+        /// <inheritdoc/>
+        public bool Contains(Message message) => message.Type != MessageType.Text ? false : message.Text.Contains(Name);
+    }
+}
