bb021f295deb60c4691ac29fe5fddf86c1434114
Aleksey <arhangelruh@gmail.com>
2020-08-26 07:44:01 +0300

    feat: add command get by type

--- src/CoctailBot/Commands/TypeCocktailCommand.cs
+++ src/CoctailBot/Commands/TypeCocktailCommand.cs
@@ -5,34 +5,36 @@ using Telegram.Bot.Types.Enums;
 using CoctailBot.Interfaces;
 using CoctailBot.Resources;
 using CoctailBot.Services;
-using CoctailBot.Logics;
-using System.Text;
+using System;
 
 namespace CoctailBot.Commands
 {
-    /// <inheritdoc cref="ITelegramCommand"/>
-    public class listAlcoholicCocktailCommand : ITelegramCommand
+    public class TypeCocktailCommand : ITelegramCommand
     {
+
         /// <inheritdoc/>
-        public string Name { get; } = listAlcoholic.Link;
+        public string Name { get; } = NameCocktail.Link;
 
         /// <inheritdoc/>
         public async Task Execute(Message message, ITelegramBotClient client)
         {
-            StringBuilder text = new StringBuilder();
             IApiService workWithApi = new ApiService();
             var chatId = message.Chat.Id;
-            var coctails = workWithApi.listAlcoholicCocktail("").GetAwaiter().GetResult();
-            text.Append("\nCocktail types:");
-            foreach (var coctail in coctails)
+            var data = message.Text.Replace("/t", "");
+            var coctails = workWithApi.GetCocktailsByRecipeAsync(data).GetAwaiter().GetResult();
+            try
             {
-                text.Append($"\n /t{coctail.strAlcoholic}");
+                foreach (var coctail in coctails.drinks)
+                {
+                    await client.SendTextMessageAsync(chatId, $"\nCocktail ID: /id{coctail.idDrink}\U0001F379 \nCocktail name:{coctail.strDrink} \n{coctail.strDrinkThumb}");
+                }
             }
-            await client.SendTextMessageAsync(chatId,text.ToString());
+            catch (NullReferenceException)
+            {
+                await client.SendTextMessageAsync(chatId, $"Cocktail not found \U0001F631");
+            }
         }
-
         /// <inheritdoc/>
-        // public bool Contains(Message message) => message.Type != MessageType.Text ? false : message.Text.Contains(Name);
         public bool Contains(Message message)
         {
             if (message != null)
